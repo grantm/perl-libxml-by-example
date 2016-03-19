80db62184bbc0d9b1ae62d31c055abf6e69fb8bc
Grant McLean <grant@mclean.net.nz>
2016-03-20 12:09:17 +1300

    use JS to wrap each 'illustration' image in a link to the image file

--- source/_static/plxbe.js
+++ source/_static/plxbe.js
@@ -5,6 +5,17 @@
 jQuery(function($) {
     'use strict';
 
+    // Wrap the img tag for each illustration in a link to just the image
+
+    $('.illustration').each(function() {
+        var $img = $(this);
+        $img.wrap( $('<a />').attr('href', $img.attr('src')) );
+    });
+
+
+    // Add in the 'Try it!' links to link from XPath expressions to load that
+    // expression in the XPath Sandbox
+
     var sandbox_base_url = '_static/xpath-sandbox/xpath-sandbox.html'
     $('.xpath-try').each(function() {
         var xpath = this.textContent;
