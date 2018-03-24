ce572e64835bcb74009acb0dc2a67e105acd983d
Grant McLean <grant@mclean.net.nz>
2018-03-24 22:15:06 +1300

    update copyright config param

--- source/conf.py
+++ source/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Perl XML::LibXML by Example'
-copyright = '2016, Grant McLean'
+copyright = '2016-2018, Grant McLean'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
